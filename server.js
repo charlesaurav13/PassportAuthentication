b8cb79a3e528d79d4750aaa40bcfb36e3f4823cb
charlesaurav13 <sauravp1236@gmail.com>
2024-02-05 16:06:32 +0530

    changed styles in view folder

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(cors("*"));
 
 // Middleware
